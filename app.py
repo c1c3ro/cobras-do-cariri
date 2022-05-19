42050f7aa2c3bcfdc8db4935326219c667440b39
c1c3ro <cicerosousa205@gmail.com>
2022-05-19 15:01:59 -0300

    tentando conectar ao banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
+
+
+conn = mysql.connector.connect(host = "mysql04-farm2.uni5.net",
+user = "bessapontes23",
+password = "marleyanos32",
+database = "bessapontes23")
+
+#Creating a connection cursor
+cursor = conn.cursor()
+ 
+#Executing SQL Statements
+cursor.execute(''' INSERT INTO COBRA (familia, especie, peconhenta, idUSUARIO) 
+                    VALUES(teste0, teste1, 0, 1) ''')
+ 
+#Saving the Actions performed on the DB
+conn.commit()
+ 
+#Closing the cursor
+cursor.close()
 
 @app.route("/")
 def index():
