24a7715319e3748206a1da7f46187786e736f677
c1c3ro <cicerosousa205@gmail.com>
2022-05-20 12:45:26 -0300

    consertando forms

---
+++ formOcorrencia.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,PasswordField, TextAreaField
+from wtforms.fields import EmailField, DateTimeField, DecimalField, FileField
+from wtforms.validators import DataRequired
+
+class OcorrenciaForm(FlaskForm):
+    localizacao = StringField('Localização', validators=[DataRequired()])
+    dataHora = DateTimeField('Data e hora', validators=[DataRequired()])
+    localizacao_lat = DecimalField("Localização - Latitude")
+    localizacao_log = DecimalField("Localização - Longitude")
+    imagem = FileField('Imagem')
+    informacao_adc = TextAreaField("Informações Adicionais")
