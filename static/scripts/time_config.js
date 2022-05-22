b4e7315a91e39c75b7cc1c0d4cfb0767b24c511e
c1c3ro <cicerosousa205@gmail.com>
2022-05-22 17:38:56 -0300

    não faço ideia do que eu fiz aí

---
+++ static/scripts/time_config.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('#hora1')[0].value = "12:00"
+document.querySelectorAll('#hora2')[0].value = "13:00"
+
+var hora1 = document.querySelectorAll('#hora1')[0].value
+var hora2 = document.querySelectorAll('#hora2')[0].value
+
+console.log(hora1)
+console.log(hora2)
