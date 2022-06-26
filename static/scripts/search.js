1f6fabfcaa997674b8d4a848f73b14785e06f6b3
c1c3ro <cicerosousa205@gmail.com>
2022-06-25 21:15:13 -0300

    alterando minha foto e a de thiago

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -3,7 +3,7 @@ function getInputValue(){
     var inputVal = document.getElementById("searchBar").value
     inputVal = inputVal.toLowerCase()
     inputVal = inputVal.replace(/\s/g, '%20')
-    var url = "/cobras/cobras/"
+    var url = "/cobras/"
     hostName = window.location.host
     url = "http://".concat(hostName, url, inputVal)
     window.location.href = url
